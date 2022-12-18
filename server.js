e8d54241fdfd2a9dd233580d138afa833506b898
Avaer Kazmer <a@modules.io>
2022-12-17 20:26:26 -0500

    Rewrite api to support binary

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import http from 'http';
 // import https from 'https';
 import path from 'path';
 import fs from 'fs';
+import url from 'url';
 import express from 'express';
 import puppeteer from 'puppeteer';
 
@@ -46,6 +47,7 @@ function makePromise() {
 
 //
 
+const args = new Map();
 const cbs = new Map();
 
 //
@@ -85,22 +87,40 @@ const _startPostbackServer = () => (async () => {
         res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
 
         if (req.method === 'OPTIONS') {
+          // console.log('postback server handle options', req.url);
           res.end();
         } else {
-          // console.log('postback to 1');
-          const url = new URL(req.url, `${req.protocol}://${req.headers.host}`);
+          // console.log('postback server got', req.url);
+          const u = new URL(req.url, `${req.protocol}://${req.headers.host}`);
           const proxyStatusCode = req.headers['x-proxy-status-code'] || 200;
-          const {searchParams} = url;
+          const {searchParams} = u;
           const id = searchParams.get('id');
           // console.log('postback', {url: req.url, headers: req.headers, id});
           
-          let cb = cbs.get(id);
-          if (cb) {
-            cbs.delete(id);
-            cb(proxyStatusCode, req, res);
+          const o = url.parse(req.url, true);
+          if (o.pathname === '/args') {
+            // console.log('postback handle args');
+            let arg = args.get(id);
+            if (arg) {
+              args.delete(id);
+              res.end(arg);
+            } else {
+              // throw new Error('no arg for id: ' + id);
+              res.status(404).end('no arg for id: ' + id);
+            }
+          } else if (o.pathname === '/cb') {
+            // console.log('postback handle cb');
+            let cb = cbs.get(id);
+            if (cb) {
+              cbs.delete(id);
+              cb(proxyStatusCode, req, res);
+            } else {
+              // throw new Error('no cb for id: ' + id);
+              res.status(404).end('no cb for id: ' + id);
+            }
           } else {
-            // throw new Error('no cb for id: ' + id);
-            res.status(404).end('no cb for id: ' + id);
+            // console.log('postback handle not found');
+            res.status(404).end('url not found');
           }
         }
       } catch(err) {
@@ -121,15 +141,44 @@ const _startPostbackServer = () => (async () => {
     });
   });
   return {
-    cbUrl: `http://127.0.0.1:${CB_PORT}`,
+    argsUrl: `http://127.0.0.1:${CB_PORT}/args`,
+    cbUrl: `http://127.0.0.1:${CB_PORT}/cb`,
     close() {
       server.close();
     },
   };
 })();
+const _readBody = req => new Promise((accept, reject) => {
+  const buffers = [];
+  const ondata = data => {
+    // console.log('args read data', data.byteLength);
+    buffers.push(data);
+  };
+  req.on('data', ondata);
+  const onend = () => {
+    const b = Buffer.concat(buffers);
+    // console.log('args read end', b.byteLength);
+    accept(b);
+    buffers.length = 0;
+    cleanup();
+  };
+  req.on('end', onend);
+  const onerror = err => {
+    // console.warn('args read error', err);
+    reject(err);
+    cleanup();
+  };
+  req.on('error', onerror);
+
+  const cleanup = () => {
+    req.removeListener('data', ondata);
+    req.removeListener('end', onend);
+    req.removeListener('error', onerror);
+  };
+});
 
 const port = parseInt(process.env.PORT, 10) || 8888;
-const compilerUrl = process.argv[2] || `https://127.0.0.1/`;
+// const compilerUrl = process.argv[2] || `https://127.0.0.1/`;
 const MAX_TIMEOUT = 120 * 1000;
 /* const start_url = process.argv[3];
 const mimeType = process.argv[4] || 'application/octet-stream';
@@ -153,38 +202,69 @@ if (compilerUrl && start_url) {
     ignoreHTTPSErrors: true,
   });
 
-  const _render = async (start_url, mimeType, args) => {
+  const _render = async (start_url, funcName, argsData) => {
+    // console.log('await new page');
+
     const page = await browser.newPage();
 
+    // console.log('renderer render', {
+    //   start_url,
+    //   funcName,
+    //   argsLength: args?.length
+    // });
+
     const id = makeId();
-    const u = compilerUrl.replace(/\/+$/, '') + '/preview.html?u=' + encodeURI(start_url) + '&type=' + encodeURIComponent(mimeType) + (args ? ('&args=' + encodeURIComponent(args)) : '') + '&cbUrl=' + encodeURI(postbackServer.cbUrl + '/?id=' + id);
-    
-    console.warn(u);
+    // const u = new URL(compilerUrl.replace(/\/+$/, '') + '/preview.html');
+    const u = new URL(start_url);
+    // ?u=' + encodeURI(start_url) + '&type=' + encodeURIComponent(mimeType) + (args ? ('&args=' + encodeURIComponent(args)) : '') + '&cbUrl=' + encodeURI(postbackServer.cbUrl + '/?id=' + id);
+    u.searchParams.set('funcName', funcName);
+    u.searchParams.set('argsUrl', postbackServer.argsUrl + '?id=' + id);
+    u.searchParams.set('cbUrl', postbackServer.cbUrl + '?id=' + id);
 
-    const promise = makePromise();
-    cbs.set(id, (statusCode, req, res) => {
-      // handleReponse(statusCode, req, res);
+    {
+      args.set(id, argsData);
+    }
+    const cbPromise = (async () => {
+      const promise = makePromise();
+      cbs.set(id, (statusCode, req, res) => {
+        // handleReponse(statusCode, req, res);
+  
+        const buffers = [];
+        const ondata = data => {
+          buffers.push(data);
+        };
+        req.on('data', ondata);
+        const onend = () => {
+          res.end();
 
-      const buffers = [];
-      req.on('data', data => {
-        buffers.push(data);
+          promise.accept({
+            contentType: req.headers['content-type'],
+            body: Buffer.concat(buffers),
+          });
+        };
+        req.on('end', onend);
+        const onerror = err => {
+          promise.reject(err);
+        };
+        req.on('error', onerror);
+        
+        const cleanup = () => {
+          req.removeListener('data', ondata);
+          req.removeListener('end', onend);
+          req.removeListener('error', onerror);
+        };
+        promise.finally(cleanup);
       });
-      req.on('end', () => {
-        res.end();
-        promise.accept({
-          contentType: req.headers['content-type'],
-          body: Buffer.concat(buffers),
-        });
-      });
-      
-      req.on('end', promise.accept);
-      req.on('error', promise.reject);
-    });
+      return await promise;
+    })();
     
     page.setDefaultNavigationTimeout(MAX_TIMEOUT);
     await page.goto(u);
 
-    const result = await promise;
+    const result = await cbPromise;
+
+    // close the page
+    await page.close();
 
     // await browser.close();
 
@@ -192,11 +272,13 @@ if (compilerUrl && start_url) {
   };
 
   const requestCache = new Map();
-  const _cachedRender = (start_url, mimeType, args, cache) => {
-    const key = start_url + ':' + mimeType + ':' + args + ':' + cache;
+  const _cachedRender = (start_url, funcName, argsData, cache) => {
+    // const key = start_url + ':' + funcName + ':' + args + ':' + cache;
+    const key = cache;
     let p = requestCache.get(key);
+    // console.log('cached render had', {start_url, funcName, had: !!p});
     if (!p) {
-      p = _render(start_url, mimeType, args);
+      p = _render(start_url, funcName, argsData);
       requestCache.set(key, p);
     }
     return p;
@@ -205,39 +287,54 @@ if (compilerUrl && start_url) {
   const app = express();
   app.all('*', async (req, res, next) => {
     // console.log('got headers', req.method, req.url, req.headers);
+    // console.log('render server got', req.url);
 
-    if (req.url === '/') {
+    const o = url.parse(req.url, true);
+    if (o.pathname === '/') {
       res.setHeader('Content-Type', 'text/html');
       const rs = fs.createReadStream(dirname + '/index.html');
       rs.pipe(res);
-    } else {
+    } else if (o.pathname === '/api') {
       // parse the URL and query string, making sure the protocol is correct
       const url = new URL(req.url, `${req.protocol}://${req.headers.host}`);
       const {searchParams} = url;
-      const start_url = searchParams.get('u');
-      const mimeType = searchParams.get('mimeType');
-      const args = searchParams.get('args') || '';
+      const start_url = searchParams.get('start_url');
+      // const mimeType = searchParams.get('mimeType');
+      const funcName = searchParams.get('funcName');
+      // const args = searchParams.get('args') || '';
       const cache = searchParams.get('cache') || '';
 
-      if (start_url && mimeType) {
+      if (start_url && funcName) {
         try {
+          // console.log('wait for args body', req.method, req.headers, start_url, funcName, cache);
+          // wait for the body
+          const argsData = await _readBody(req);
+          // console.log('got args body 0');
+          // console.log('got args body 1', argsData?.length);
           const {
             contentType,
             body,
-          } = await _cachedRender(start_url, mimeType, args, cache);
+          } = await _cachedRender(start_url, funcName, argsData, cache);
+          // console.log('renderer got result', contentType, body?.length);
           res.setHeader('Content-Type', contentType);
           res.setHeader('Access-Control-Allow-Origin', '*');
           res.setHeader('Access-Control-Allow-Methods', '*');
           res.setHeader('Access-Control-Allow-Headers', '*');
           res.end(body);
         } catch(err) {
-          console.warn(err.stack);
+          // console.warn('render server caught error', err.stack);
           res.setHeader('Content-Type', 'text/plain');
           res.status(500).send(err.stack);
         }
       } else {
-        res.status(404).end('invalid query: ' + JSON.stringify({start_url, mimeType}));
+        // console.warn('render server invalid query', {
+        //   start_url,
+        //   funcName,
+        // });
+        res.status(404).end('invalid query: ' + JSON.stringify({start_url, funcName}));
       }
+    } else {
+      res.status(404).end('not found');
     }
   });
 
